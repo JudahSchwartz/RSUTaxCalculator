1d7ceee271d5a4131544b6e56802dfe81a29bc56
ShacharKidor <skidorball@paloaltonetworks.com>
2023-03-07 12:56:39 +0200

    dded the submit Button Componenet

---
+++ Webapp/rsu_tax_calculator/src/Components/SubmitButton.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+
+class SubmitButton extends Component {
+    
+    handleChange = (event) => {
+        // add an API call to the backend
+        // Also get the info from all other components to transferr to the backend
+        //this.setState({chosenOption: event.target.value})
+        console.log("Submit Button Clicked")
+    }
+
+    render() {
+        return (
+            <div>
+                <br />
+                <br />
+                <button onClick={this.handleChange}>
+                    Submit
+                </button>  
+            </div>
+        )
+    }
+}
+
+export default SubmitButton
