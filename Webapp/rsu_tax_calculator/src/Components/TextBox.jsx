01fdfffd166613657f441dbdf9b4299e6518c7a0
GuyAfik <guyafik11@gmail.com>
2023-03-21 11:49:30 +0200

    add text-boxes validations to integer

--- Webapp/rsu_tax_calculator/src/Components/TextBox.jsx
+++ Webapp/rsu_tax_calculator/src/Components/TextBox.jsx
@@ -7,6 +7,7 @@ class TextBox extends Component {
             <div>
                 <h3>{this.props.textBox.headerValue}</h3>
                 <input onChange={(event) => this.props.onChangeBoxValue(event, this.props.textBox)}/>
+                {this.props.textBox.errorMessage && <div className="error">{this.props.textBox.errorMessage}</div>}
             </div>
         )
     }
